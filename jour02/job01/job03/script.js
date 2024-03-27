e9bd48807bb3d42630ce848845dfb6ab2ecf58cd
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-03-27 21:50:14 +0100

    ajout du job03

---
+++ jour02/job01/job03/script.js
@@ -0,0 +1,8 @@
+
+const button = document.getElementById("button");
+const p = document.getElementById("count");
+var counter = 0
+button.addEventListener("click",function addOne ()  {
+    counter ++;
+p.textContent = `Click count: ${counter}`;
+});
